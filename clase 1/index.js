2163c3fb5e2042b840474286580355bd1d62a8e8
ignacio skibski <nanoskibski@gmail.com>
2022-09-05 23:25:32 -0300

    clase 2

--- clase 1/index.js
+++ clase 1/index.js
@@ -1,8 +1,67 @@
-//declaracion de una variable
+/* //declaracion de una variable
 let edad = 45;
 let numeroUnoBonito;
 
 //asignacion de variable
 edad= 23;
 
-console.log(edad);
+console.log(edad); */
+
+/* let numeroUno= 5;
+let numeroDos= 10;
+
+let suma= numeroUno + numeroDos;
+let resta= numeroUno - numeroDos
+
+console.log(suma);
+console.log(resta); */
+
+/* let nombre= "andres";
+let apellido= "damaTO";
+
+let mensaje= nombre + " " + apellido;
+
+console.log(mensaje);
+let mensaje1= `${nombre} ${apellido}`;
+console.log(mensaje1); */
+
+/* let nombre= prompt("ingresa tu nombre");
+alert(nombre); */
+
+/* let nombre = prompt("ingrese un nombre");
+
+if (nombre === "camila"){
+    alert("hola camila")
+} */
+
+
+/* let numero = 10
+
+if (numero===10){
+    console.log("eres el 10");
+}else{
+    console.log("no eres el 10");
+} */
+
+/* let numero=10;
+
+if (numero===20){
+    console.log("eres el 20");
+}else if (numero===30){
+    console.log("eres el 30");
+}else{
+    console.log("no eres ni el 30 ni 20");
+}
+console.log("final"); */
+
+
+/* let nombre= prompt("ingrese el nombre")
+
+if (nombre===""){
+    alert("no ingresaste ningun nombre")
+}else {
+    let mensaje= `bienvenido ${nombre}`;
+    alert(mensaje);
+} */
+
+
